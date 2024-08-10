327f818b249aaeadf1a930b583bf9db2b175b269
“lizandambs” <lizndambs@gmail.com>
2024-08-09 21:36:11 -0400

    terraform

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "ami" {
+  description = "amazon machine image"
+  type        = string
+  default     = "ami-03972092c42e8c0ca"
+}
+variable "instanceType" {
+  default = "t2.small"
+}
+variable "region_name" {
+  # default = "us-east-1" 
+}
+variable "profile_name" {
+  default = "default"
+
+}
